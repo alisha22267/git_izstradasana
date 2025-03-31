89f80861e6a9c03b2110589945fa64f88ebdc026
kolobanova <alinakolobanova22@gmail.com>
2025-03-31 14:34:13 +0300

    Metode ievests cikla

--- src/git_izsrtradasana/kaulins.java
+++ src/git_izsrtradasana/kaulins.java
@@ -3,12 +3,17 @@ package git_izsrtradasana;
 import java.util.Random;
 
 public class kaulins {
-
-	public static void main(String[] args) {
+	static void mestKaulinu(int reiz) {
 		int sk;
 		Random rand =  new Random();
-		sk = rand.nextInt(6)+1;
-		System.out.println("Skaitlis uzkrita: " +sk);
+		for(int i=0; i<reiz; i++) {
+			sk = rand.nextInt(6)+1;
+			System.out.println("Skaitlis uzkrita: " +sk);
+		}
+	}
+
+	public static void main(String[] args) {
+		mestKaulinu(3);
 
 	}
 
